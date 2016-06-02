50334be6e6aee39467ed49c42754cd3a254f020d
lorenzop <lorenzo@poixson.com>
2016-06-02 14:11:32 -0400

    added 404 page file

---
+++ src/pages/page_404.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpPortal\pages;
+
+use pxn\phpPortal\Website;
+
+use pxn\phpUtils\Defines;
+
+
+class page_404 {
+
+
+
+	public function getPageContents() {
+		$FailedPage = Website::get()
+			->getArg(Defines::KEY_FAILED_PAGE);
+		return
+"<center>
+	<h1>404 - Page Not Found!</h1>
+	<h3>Page: {$FailedPage}</h3>
+</center>
+";
+	}
+
+
+
+}
