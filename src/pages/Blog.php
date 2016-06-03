fd0adcebeaac6f9c8d8035fbefefb7ff296e75e4
lorenzop <lorenzo@poixson.com>
2016-06-03 17:00:37 -0400

    moved and finished getEntries() and getComments() functions to new Blog_Queries class

--- src/pages/Blog.php
+++ src/pages/Blog.php
@@ -10,33 +10,31 @@ namespace pxn\phpPortal\pages;
 
 use pxn\phpPortal\Website;
 
-use pxn\phpUtils\pxdb\dbPool;
-use pxn\phpUtils\pxdb\dbConn;
-
-use pxn\phpUtils\Numbers;
-
 
 abstract class Blog extends \pxn\phpPortal\Page {
 
-	protected static $dbName = 'main';
-
-	protected $dateFormat = \DATE_RFC2822;
+	protected $pool = NULL;
+	protected $queries = NULL;
 
 
 
 	public function getPageContents() {
+//self::UpdateCommentCounts(); exit(1);
 		$tpl = $this->getBlogTpl();
-		$entryId = (int) Website::get()->getArg(1);
-		if ($entryId <= 0) {
-			$entryId = NULL;
-		}
-		$entries = $this->getEntries($entryId);
+		$website = Website::get();
+		$entryId = (int) $website->getArg(1);
+		$entries = $this->getQueriesClass()
+			->getEntries($entryId);
 		if ($entries == NULL) {
 			fail('Failed to get blog entries!');
 			exit(1);
 		}
+		$comments = $this->getQueriesClass()
+			->getComments($entryId);
 		return $tpl->render([
-			'entries' => $entries
+			'singleId' => (int) $entryId,
+			'entries'  => $entries,
+			'comments' => $comments
 		]);
 	}
 
@@ -45,114 +43,11 @@ abstract class Blog extends \pxn\phpPortal\Page {
 	public function getBlogTpl() {
 		return $this->getTpl('blog');
 	}
-
-
-
-	protected function getEntries($entryId=NULL) {
-		$db = $this->doQuery($entryId);
-		if ($db == NULL) {
-			return NULL;
+	public function getQueriesClass() {
+		if ($this->queries == NULL) {
+			$this->queries = new Blog_Queries($this->pool);
 		}
-		$entries = [];
-		$rowNum = 0;
-		while ($db->hasNext()) {
-			$rowNum++;
-			$timestamp = $db->getInt('timestamp');
-			$timeNow = \time();
-			$timeSinceStr =
-				($timestamp > 0)
-				? Numbers::SecondsToText($timeNow - $timestamp, FALSE, 2, 0.9)
-				: '';
-			$entry = [
-				'rowNum'        => $rowNum,
-				'id'            => $db->getInt('entry_id'),
-				'title'         => $db->getString('title'),
-				'body'          => $db->getString('body'),
-				'dateFormatted' => \date($this->dateFormat, $timestamp),
-				'timeSinceText' => $timeSinceStr,
-				'commentCount'  => $db->getInt('comment_count')
-			];
-			$entries[] = $entry;
-		}
-		$db->release();
-		return $entries;
-	}
-	protected function doQuery($entryId=NULL) {
-		$db = dbPool::get(self::$dbName, dbConn::ERROR_MODE_EXCEPTION);
-		if ($db == NULL) {
-			return NULL;
-		}
-		try {
-			$sql = $this->getSql($entryId);
-			$db->Prepare($sql);
-			$db->Execute();
-		} catch (\PDOException $e) {
-			fail("Query failed: {$sql}", $e);
-			exit(1);
-		}
-		return $db;
-	}
-	protected function getSql($entryId=NULL) {
-		$entryId = (int) $entryId;
-		return "SELECT `entry_id`, `title`, `body`, `comment_count`, ".
-			"UNIX_TIMESTAMP(`timestamp`) AS `timestamp` ".
-//			"( SELECT COUNT(*) FROM `__TABLE__comments` ".
-//				"WHERE `context` = 'blog' AND ".
-//				"`context_id` = `__TABLE__blog_entries`.`entry_id` ".
-//				") AS `comment_count` ".
-			"FROM `__TABLE__blog_entries` ".
-			"WHERE `timestamp` <= NOW() ".
-			( $entryId > 0 ? "AND `entry_id` = {$entryId} " : '' ).
-			"ORDER BY `timestamp` DESC, `entry_id` DESC ".
-			"LIMIT 5";
-	}
-
-
-
-	public static function UpdateCommentCounts($entry_id=NULL) {
-		if ($entry_id !== NULL) {
-			$entry_id = (int) $entry_id;
-			if ($entry_id <= 0) {
-				$funcName = __function__;
-				fail("Invalid entry_id provided to {$funcName} function: {$entry_id}");
-				exit(1);
-			}
-		}
-		$dbQuery  = dbPool::get(self::$dbName, dbConn::ERROR_MODE_EXCEPTION);
-		$dbUpdate = dbPool::get(self::$dbName, dbConn::ERROR_MODE_EXCEPTION);
-		try {
-			$sql = "SELECT `entry_id` FROM `__TABLE__blog_entries`";
-			$dbQuery->Execute($sql);
-			$count = 0;
-			while ($dbQuery->hasNext()) {
-				$id = $dbQuery->getInt('entry_id');
-				if ($id <= 0) {
-					fail('Invalid entry_id value in blog_entries table!');
-					exit(1);
-				}
-				try {
-					$sql = "UPDATE `__TABLE__blog_entries` SET ".
-						"`comment_count` = ( ".
-							"SELECT COUNT(*) FROM `__TABLE__comments` ".
-							"WHERE `context` = 'blog' AND `context_id` = :id ".
-						") ".
-						"WHERE `entry_id` = :id LIMIT 1";
-					$dbUpdate->Prepare($sql);
-					$dbUpdate->setInt(':id', $id);
-					$dbUpdate->Execute();
-				} catch (\PDOException $e) {
-					fail("Query failed: {$sql}", $e);
-					exit(1);
-				}
-				$dbUpdate->clean();
-				$count++;
-			}
-		} catch (\PDOException $e) {
-			fail("Query failed: {$sql}", $e);
-			exit(1);
-		}
-		$dbUpdate->release();
-		$dbQuery->release();
+		return $this->queries;
 	}
 
 
---
+++ src/pages/Blog_Queries.php
@@ -0,0 +1,192 @@
+<?php
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpPortal\pages;
+
+use pxn\phpUtils\pxdb\dbPool;
+use pxn\phpUtils\pxdb\dbConn;
+
+use pxn\phpUtils\Numbers;
+
+
+class Blog_Queries {
+
+	public $dateFormat = \DATE_RFC2822;
+
+	public $pool = NULL;
+
+
+
+	public function __construct($pool) {
+		if ($pool != NULL) {
+			$this->pool = $pool;
+		}
+	}
+
+
+
+	public function getEntries($entryId=NULL) {
+		$entryId = (int) $entryId;
+		$db = dbPool::get($this->pool, dbConn::ERROR_MODE_EXCEPTION);
+		if ($db == NULL) {
+			return NULL;
+		}
+		$entries = [];
+		$rowNum = 0;
+		try {
+			$sql = $this->getEntriesSQL($entryId);
+			$db->Prepare($sql);
+			if ($entryId > 0) {
+				$db->setInt(':id', $entryId);
+			}
+			$db->Execute();
+			while ($db->hasNext()) {
+				$rowNum++;
+				$timestamp = $db->getInt('timestamp');
+				$timeNow = \time();
+				$timeSinceStr =
+					($timestamp > 0)
+					? Numbers::SecondsToText($timeNow - $timestamp, FALSE, 2, 0.9)
+					: '';
+				$entry = [
+					'rowNum'        => $rowNum,
+					'id'            => $db->getInt('entry_id'),
+					'title'         => $db->getString('title'),
+					'body'          => $db->getString('body'),
+					'dateFormatted' => \date($this->dateFormat, $timestamp),
+					'timeSinceText' => $timeSinceStr,
+					'commentCount'  => $db->getInt('comment_count')
+				];
+				$entries[] = $entry;
+			}
+		} catch (\PDOException $e) {
+			fail("Query failed: {$sql}", $e);
+			exit(1);
+		}
+		$db->release();
+		return $entries;
+	}
+	public function getEntriesSQL($entryId=NULL) {
+		$entryId = (int) $entryId;
+		return "SELECT `entry_id`, `title`, `body`, `comment_count`, ".
+			"UNIX_TIMESTAMP(`timestamp`) AS `timestamp` ".
+//			"( SELECT COUNT(*) FROM `__TABLE__comments` ".
+//				"WHERE `context` = 'blog' AND ".
+//				"`context_id` = `__TABLE__blog_entries`.`entry_id` ".
+//				") AS `comment_count` ".
+			"FROM `__TABLE__blog_entries` ".
+			"WHERE `timestamp` <= NOW() ".
+			( $entryId > 0 ? "AND `entry_id` = :id " : '' ).
+			"ORDER BY `timestamp` DESC, `entry_id` DESC ".
+			"LIMIT 5";
+	}
+
+
+
+	public function getComments($entryId=NULL) {
+		$entryId = (int) $entryId;
+		if ($entryId <= 0) {
+			return NULL;
+		}
+		$db = dbPool::get($this->pool, dbConn::ERROR_MODE_EXCEPTION);
+		if ($db == NULL) {
+			return NULL;
+		}
+		$comments = [];
+		$commentNum = 0;
+		try {
+			$sql = $this->getCommentsSQL($entryId);
+			$db->Prepare($sql);
+			$db->setInt(':id', $entryId);
+			$db->Execute();
+			while ($db->hasNext()) {
+				$commentNum++;
+				$timestamp = $db->getInt('timestamp');
+				$timeNow = \time();
+				$timeSinceStr =
+					($timestamp > 0)
+					? Numbers::SecondsToText($timeNow - $timestamp, FALSE, 2, 0.9)
+					: '';
+				$comment = [
+					'commentNum'    => $commentNum,
+					'id'            => $db->getInt('comment_id'),
+					'author'        => $db->getString('author'),
+					'body'          => $db->getString('body'),
+					'dateFormatted' => \date($this->dateFormat, $timestamp),
+					'timeSinceText' => $timeSinceStr
+				];
+				$comments[] = $comment;
+			}
+		} catch (\PDOException $e) {
+			fail("Query failed: {$sql}", $e);
+			exit(1);
+		}
+		$db->release();
+		return $comments;
+	}
+	public function getCommentsSQL($entryId=NULL) {
+		$entryId = (int) $entryId;
+		return "SELECT `comment_id`, `body`, `author`, ".
+			"UNIX_TIMESTAMP(`timestamp`) AS `timestamp` ".
+			"FROM `__TABLE__comments` ".
+			"WHERE `context` = 'blog' AND `context_id` = :id ".
+			"ORDER BY `timestamp` DESC, `comment_id` DESC ".
+			"LIMIT 1000";
+	}
+
+
+
+	public function UpdateCommentCounts($entry_id=NULL) {
+		if ($entry_id !== NULL) {
+			$entry_id = (int) $entry_id;
+			if ($entry_id <= 0) {
+				$funcName = __function__;
+				fail("Invalid entry_id provided to {$funcName} function: {$entry_id}");
+				exit(1);
+			}
+		}
+		$dbQuery  = dbPool::get($this->pool, dbConn::ERROR_MODE_EXCEPTION);
+		$dbUpdate = dbPool::get($this->pool, dbConn::ERROR_MODE_EXCEPTION);
+		try {
+			$sql = "SELECT `entry_id` FROM `__TABLE__blog_entries`";
+			$dbQuery->Execute($sql);
+			$count = 0;
+			while ($dbQuery->hasNext()) {
+				$id = $dbQuery->getInt('entry_id');
+				if ($id <= 0) {
+					fail('Invalid entry_id value in blog_entries table!');
+					exit(1);
+				}
+				try {
+					$sql = "UPDATE `__TABLE__blog_entries` SET ".
+						"`comment_count` = ( ".
+							"SELECT COUNT(*) FROM `__TABLE__comments` ".
+							"WHERE `context` = 'blog' AND `context_id` = :id ".
+						") ".
+						"WHERE `entry_id` = :id LIMIT 1";
+					$dbUpdate->Prepare($sql);
+					$dbUpdate->setInt(':id', $id);
+					$dbUpdate->Execute();
+				} catch (\PDOException $e) {
+					fail("Query failed: {$sql}", $e);
+					exit(1);
+				}
+				$dbUpdate->clean();
+				$count++;
+			}
+		} catch (\PDOException $e) {
+			fail("Query failed: {$sql}", $e);
+			exit(1);
+		}
+		$dbUpdate->release();
+		$dbQuery->release();
+	}
+
+
+
+}
