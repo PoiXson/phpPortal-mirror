7b26b1a7d6d16937c8b03b1138f1ae9fb2cfa509
lorenzop <lorenzo@poixson.com>
2016-08-19 00:03:59 -0400

    added WebRender class

---
+++ src/WebRender.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpPortal;
+
+
+class WebRender extends \pxn\phpUtils\app\Render {
+
+
+
+
+
+
+}
