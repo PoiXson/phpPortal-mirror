4ac58d674725988bb9d0e3f6e3e6f160345d0f37
lorenzop <lorenzo@poixson.com>
2016-08-19 03:52:08 -0400

    finished RenderMain class

--- src/render/RenderMain.php
+++ src/render/RenderMain.php
@@ -8,9 +8,9 @@
  */
 namespace pxn\phpPortal\render;
 
-//use pxn\phpUtils\Config;
-//use pxn\phpUtils\Defines;
-//use pxn\phpUtils\System;
+use pxn\phpUtils\Config;
+use pxn\phpUtils\Defines;
+use pxn\phpUtils\System;
 
 
 class RenderMain extends \pxn\phpPortal\WebRender {
@@ -23,41 +23,35 @@ class RenderMain extends \pxn\phpPortal\WebRender {
 
 
 
-/*
 	public function doRender() {
-		$isShell = System::isShell();
-		// get page contents
-		$pageContents = self::$website->getPageContents();
+		if (System::isShell()) {
+			$name = $this->getName();
+			fail("Cannot use a WebRender class in this mode! {$name}"); ExitNow(1);
+		}
+		if (! $this->app instanceof \pxn\phpPortal\WebApp) {
+			fail('App not instance of WebApp!'); ExitNow(1);
+		}
+		// get page contents (has internal buffering)
+		$pageContents = $this->app->getPageContents();
 		// get page title
-		$pageTitle = self::$website->getPageName();
-		if ($pageTitle instanceof Page) {
-			$pageTitle = $pageTitle->getTitle();
-		} else {
-			$pageTitle = (String) $pageTitle;
+		$pageTitle = $this->app->getPageTitle();
+		if (\is_callable($pageTitle)) {
+			$pageTitle = $pageTitle();
 		}
-		$title = \str_replace(
-				'{pagetitle}',
-				$pageTitle,
-				Config::get(Defines::KEY_SITE_TITLE)
-		);
-
-		// shell mode
-		if ($isShell) {
-			echo "\n";
-//			if (!empty($pageTitle)) {
-//				echo " == Title: {$pageTitle} == \n";
-//			}
-			echo "\n{$pageContents}\n";
-			@\ob_flush();
-			return;
-		}
-
+		$pageTitle = (string) $pageTitle;
+//		$pageTitle = \mb_str_replace(
+//				'{pagetitle}',
+//				$pageTitle,
+//				Config::get(Defines::KEY_SITE_TITLE)
+//		);
+		// page icon
 		$iconFile  = Config::get(Defines::KEY_FAV_ICON);
-		// load template file
-		$tpl = self::$website->getTpl('main');
+		// load global template file
+		$tpl = $this->app->getTpl('main');
 		// start rendering html
 		$CRLF = Defines::CRLF;
 		$TAB  = Defines::TAB;
+
 		echo
 			'<!DOCTYPE html>'.$CRLF.
 			'<html lang="en">'.$CRLF.
@@ -65,7 +59,7 @@ class RenderMain extends \pxn\phpPortal\WebRender {
 			'<meta charset="utf-8" />'.$CRLF.
 			'<meta http-equiv="X-UA-Compatible" content="IE=edge" />'.$CRLF.
 			'<meta name="viewport" content="width=device-width, initial-scale=1" />'.$CRLF.
-			"<title>{$title}</title>".$CRLF.
+			"<title>{$pageTitle}</title>".$CRLF.
 
 			// fav icon
 			(empty($iconFile) ? '' :
@@ -82,7 +76,9 @@ class RenderMain extends \pxn\phpPortal\WebRender {
 
 			'</head>'.$CRLF.
 			'<body>'.$CRLF;
+
 		@\ob_flush();
+
 		// render with twig
 		echo $tpl->render([
 				'PageContents' => &$pageContents
@@ -90,8 +86,20 @@ class RenderMain extends \pxn\phpPortal\WebRender {
 		echo
 			'</body>'.$CRLF.
 			'</html>';
+
 		@\ob_flush();
 	}
+/*
+		// shell mode
+		if ($isShell) {
+			echo "\n";
+//			if (!empty($pageTitle)) {
+//				echo " == Title: {$pageTitle} == \n";
+//			}
+			echo "\n{$pageContents}\n";
+			@\ob_flush();
+			return;
+		}
 */
 
 
