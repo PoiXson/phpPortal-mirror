9574dfe2a2de61afcf50257eabf075c8cd6d9e05
lorenzop <lorenzo@poixson.com>
2017-07-28 12:36:30 -0400

    added Wiki page class

---
+++ src/pages/wiki/Wiki.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpPortal\pages\wiki;
+
+use Michelf\Markdown;
+use Michelf\MarkdownExtra;
+
+
+// https://michelf.ca/projects/php-markdown/extra/
+abstract class Wiki extends \pxn\phpPortal\Page {
+
+	protected $allowExtra = FALSE;
+
+
+
+	protected abstract function getPageData();
+
+
+
+	public function getPageContents() {
+		$data = $this->getPageData();
+		if (empty($data)) {
+			return 'This page is currently blank.';
+		}
+		$markdown = $this->getMarkdown();
+		$html = $markdown->transform($data);
+		return $html;
+	}
+
+
+
+	public function getMarkdown() {
+		$markdown = (
+			$this->allowExtra
+			? new MarkdownExtra()
+			: new Markdown()
+		);
+		$markdown->enhanced_ordered_list = TRUE;
+		return $markdown;
+	}
+	protected function setAllowExtra($allow=TRUE) {
+		$this->allowExtra = $allow;
+	}
+
+
+
+}
