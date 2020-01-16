d314c2aeb7f6a20fb41b2441a170462c4d6f922b
lorenzop <lorenzo@poixson.com>
2020-01-16 16:47:19 -0500

    added a simple UserManager trait

---
+++ src/UserManager.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2020
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link https://poixson.com/
+ */
+namespace pxn\phpPortal;
+
+use pxn\phpUtils\GeneralUtils;
+use pxn\phpUtils\SystemUtils;
+use pxn\phpUtils\Strings;
+use pxn\phpUtils\San;
+
+
+trait UserManager {
+
+	protected $user = NULL;
+
+
+
+	protected function initUserManager(): void {
+		$sessionName = $this->getSessionName();
+		if (!empty($sessionName)) {
+			\session_name($sessionName);
+		}
+		\session_start();
+//TODO: logging
+//echo \session_id();
+		if (isset($_SESSION['username'])) {
+			$username = $_SESSION['username'];
+			if (!empty($username)) {
+				$this->user = $username;
+			}
+		}
+	}
+
+
+
+	public function isLoggedIn(): bool {
+		return ($this->user !== NULL);
+	}
+
+
+
+	public function getSessionName(): ?string {
+		return NULL;
+	}
+
+
+
+}
