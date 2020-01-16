7c7a326c45b9e36c388613e6775e10bdfe0b21ae
lorenzop <lorenzo@poixson.com>
2020-01-16 17:47:38 -0500

    finished first version of page_login and page_logout classes

---
+++ src/pages/page_login.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2020
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link https://poixson.com/
+ */
+namespace pxn\phpPortal\pages;
+
+use pxn\phpUtils\GeneralUtils;
+
+
+class page_login extends \pxn\phpPortal\Page {
+
+
+
+	public function __construct(\pxn\phpPortal\WebApp $app) {
+		parent::__construct($app);
+		$action = GeneralUtils::getVar('action', 's', 'p');
+		if (!empty($action)) {
+			$username = GeneralUtils::getVar('username', 's', 'p');
+			$password = GeneralUtils::getVar('password', 's', 'p');
+			$result = \pam_auth($username, $password, $err);
+			if ($result === TRUE) {
+				$_SESSION['username'] = $username;
+				$this->app->getRender()->forwardTo('/');
+			} else {
+//TODO: logging
+				echo "$err<br />\n";
+			}
+		}
+	}
+
+
+
+	public function getPageTitle(): string {
+		return 'Login';
+	}
+
+
+
+	public function getContents(): string {
+		$tpl = $this->getTpl('pages/login.twig');
+		return $tpl->render();
+	}
+
+
+
+}
---
+++ src/pages/page_logout.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2020
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link https://poixson.com/
+ */
+namespace pxn\phpPortal\pages;
+
+
+class page_logout extends \pxn\phpPortal\Page {
+
+
+
+	public function __construct(\pxn\phpPortal\WebApp $app) {
+		parent::__construct($app);
+		\session_destroy();
+		$this->app->getRender()->forwardTo('http://127.0.0.1:9999/');
+	}
+
+
+
+	public function getPageTitle(): string {
+		return 'Logout';
+	}
+
+
+
+	public function getContents(): string {
+		throw new \RuntimeException('logout page doesn\'t have contents');
+	}
+
+
+
+}
