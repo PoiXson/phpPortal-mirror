5293a464650de8ee4a1979e1922ba5a029b9143b
lorenzop <lorenzo@poixson.com>
2021-03-29 22:52:24 -0400

    cleaned up and improved Router.php

--- src/Router.php
+++ src/Router.php
@@ -9,6 +9,8 @@
 namespace pxn\phpPortal;
 
 use pxn\phpUtils\utils\StringUtils;
+use pxn\phpUtils\exceptions\RequiredArgumentException;
+use pxn\phpUtils\exceptions\FileNotFoundException;
 
 
 class Router {
@@ -34,50 +36,53 @@ class Router {
 
 
 	public function getCurrentPage(): string {
-		if (!empty($this->page_current)) return $this->page_current;
+		if (!empty($this->page_current))
+			return $this->page_current;
 		// requested page
 		if (!empty($_SERVER['REQUEST_URI'])) {
-			$this->page_current = self::san_route($_SERVER['REQUEST_URI']);
-			if (!empty($this->page_current)) return $this->page_current;
+			$query = self::san_query( $_SERVER['REQUEST_URI'] );
+			if (!empty($query)) { $this->page_current = $query; return $query; }
 		}
 		// default page
 		if (!empty($this->page_default)) {
-			$this->page_current = self::san_route($this->page_default);
-			if (!empty($this->page_current)) return $this->page_current;
+			$query = self::san_query( $this->page_default );
+			if (!empty($query)) { $this->page_current = $query; return $query; }
 		}
 		// 404 page not found
-		if (empty($this->page_current))
-			$this->page_current = '404';
+		$this->page_current = '404';
 		return $this->page_current;
 	}
 
-	public function getPage(string $path=null): Page {
+	public function getPage(string $query=null): Page {
+		// page already loaded
 		if ($this->page_loaded != null)
 			return $this->page_loaded;
-		if (empty($path)) {
-			$path = $this->getCurrentPage();
-		}
-		$path = StringUtils::trim(text: $path, remove: '/');
-		if (empty($path)) throw new \RuntimeException('Failed to find current page');
-		$pos = \mb_strpos(haystack: $path, needle: '/');
+		$query = self::san_query($query);
+		// current page
+		if (empty($query))
+			$query = $this->getCurrentPage();
+		if (empty($query)) throw new \RuntimeException('Failed to find current page');
+		$pos = \mb_strpos(haystack: $query, needle: '/');
 		$name = null;
 		if ($pos === false) {
-			$name = $path;
+			$name = $query;
 			$path = '';
 		} else {
-			$name = \mb_substr($path, 0, $pos);
-			$path = \mb_substr($path, $pos+1);
+			$name = \mb_substr($query, 0, $pos);
+			$path = \mb_substr($query, $pos+1);
 		}
 		if (!empty($name)) {
 			// known page
 			if (isset($this->pages[$name])) {
-//TODO: probably not needed
-//				if ($this->pages[$name] instanceof Page) {
-//					$this->page = $this->pages[$name];
-//					return $this->page;
-//				}
+				// probably not needed but just in case
+				if ($this->pages[$name] instanceof Page) {
+					$this->page = $this->pages[$name];
+					return $this->page;
+				}
 				// load page
 				$clss = $this->pages[$name];
+				if (!\class_exists($clss))
+					throw new FileNotFoundException("class: $clss");
 				$this->page = new $clss(app: $this->app, args: $path);
 				return $this->page;
 			}
@@ -91,7 +96,7 @@ class Router {
 				return $this->routers[$name]->getPage($path);
 			}
 		}
-		// 404 page not found
+		// 404 page itself not found
 		if ($name == '404') {
 //TODO: logging
 			header("HTTP/1.0 404 Not Found");
@@ -100,26 +105,25 @@ class Router {
 		}
 		// get 404 page
 		{
-			$rt = $this->app->getRouter();
-			return $rt->getPage("404/$name/$path");
+			$router = $this->app->getRouter();
+			return $router->getPage("404/$query");
 		}
 	}
 
 
 
-	public function add(string $name, string|array|Router $page): Router {
+	public function addMany(array $pages): Router {
+		foreach ($pages as $key => $value) {
+			$this->add(name: $key, page: $value);
+		}
+		return $this;
+	}
+	public function add(string $name, string|Router $page): Router {
 		if (empty($name))  throw new RequiredArgumentException('name');
 		if ($page == null) throw new RequiredArgumentException('page');
 		// add router
 		if ($page instanceof Router) {
 			return $this->add_router(name: $name, router: $page);
-		}
-		// add many
-		if (\is_array($page)) {
-			foreach ($page as $key => $value) {
-				$this->add(name: $key, page: $value);
-			}
-			return $this;
 		}
 		// add page class
 		if (\is_string($page)) {
@@ -131,16 +135,18 @@ class Router {
 		// unknown type
 		throw new \RuntimeException("Unknown page type for: $name");
 	}
-	public function add_router(string $name, Router $router=null): Router {
-		if (isset($this->routers[$name]))
-			if ($router != null && $router !== $this->routers[$name])
-				throw new \RuntimeException("Router already registered: $name");
-		// new router
-		if ($router == null) {
-			$router = new Router(app: $this->app, name: $name);
+	public function router(string $name): Router {
+		if (empty($name)) throw new \RequiredArgumentException('router name');
+		if (!isset($this->routers[$name])) {
+			$this->routers[$name] = new self(app: $this->app, name: $name);
 		}
+		return $this->routers[$name];
+	}
+	public function add_router(string $name, Router $router): void {
+		if (isset($this->routers[$name]))
+			if ($router !== $this->routers[$name])
+				throw new \RuntimeException("Router already registered: $name");
 		$this->routers[$name] = $router;
-		return $router;
 	}
 	public function add_page(string $name, string $clss): void {
 		if (isset($this->pages[$name]))
@@ -151,23 +157,29 @@ class Router {
 
 
 
-	public function getPagesArray(): array {
+	public function get_pages_array(): array {
 		return $this->pages;
 	}
-	public function getRoutersArray(): array {
+	public function get_routers_array(): array {
 		return $this->routers;
 	}
 
 
 
-	public function def_page(string $name): void {
+	// default page
+	public function defpage(string $name): void {
 		$this->page_default = $name;
 	}
 
 
 
-	public static function san_route(string $route) {
-		return \pxn\phpUtils\utils\SanUtils::path_safe(path: $route);
+	public static function san_query(?string $query) {
+		if (empty($query))
+			return null;
+		return
+			\pxn\phpUtils\utils\SanUtils::path_safe(
+				path: StringUtils::trim(text: $query, remove: '/')
+			);
 	}
 
 
