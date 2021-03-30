29f9ae95a0f3e622d318e59958c34e6b89ac5c32
lorenzop <lorenzo@poixson.com>
2021-03-29 23:16:41 -0400

    cleaned up and finished page_404.php

--- src/pages/page_404.php
+++ src/pages/page_404.php
@@ -5,40 +5,41 @@
  * @license GPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
 namespace pxn\phpPortal\pages;
+
+use pxn\phpPortal\WebApp;
 
 
 class page_404 extends \pxn\phpPortal\Page {
 
-	protected $missingPage = NULL;
+//TODO
+//	protected ?string $page_missing;
 
 
 
-	public function __construct(\pxn\phpPortal\WebApp $app, ?string $missingPage=NULL) {
-		parent::__construct($app);
-		$this->missingPage = $missingPage;
+	public function init(): void {
+		parent::init();
+		header("HTTP/1.0 404 Not Found");
+//TODO: san this
+//		$this->page_missing = $missing;
 	}
 
 
 
-	public function getPageTitle(): string {
-		return '404 - Page Not Found!';
-	}
-
-
-
-	public function getContents(): string {
-		$missingPageName = self::getPageName($this->missingPage);
-		return <<<EOF
-<center>
-	<h1>404 - Page Not Found!</h1>
-	<h3>Page: {$missingPageName}</h3>
-</center>
-EOF;
+	public function render(): void {
+		parent::render();
+		$twig = $this->getTwig();
+		// load template
+		$tpl = $twig->load('pages/404.twig');
+		// tags
+		$tags = $this->getTags();
+		$tags['page_title'] = '404 Page Not Found';
+		$tags['page_missing'] = $this->route ?? '';
+		// render page
+		$tpl->display($tags);
 	}
 
 
 
 }
-*/
