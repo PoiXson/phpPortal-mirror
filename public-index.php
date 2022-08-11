666d2949f9438919542a48b5c1fb0225f4cfb55b
lorenzop <lorenzo@poixson.com>
2022-08-11 09:07:16 -0400

    finished new index.php

--- public-index.php
+++ public-index.php
@@ -1,14 +1,29 @@
-<?php
-/*
-// Place this file in public/ on your web server
+<?php declare(strict_types=1);
 
-if (!\file_exists('../pxnloader.php')) {
-	echo '<h2>File not found: pxnloader.php, run <i>composer update</i></h2>';
-	exit(1);
+// class loader
+$loader = null;
+if (\file_exists(__DIR__.'/../vendor/autoload.php')) {
+	$loader = require(__DIR__.'/../vendor/autoload.php');
+} else
+if (\file_exists(__DIR__.'/../website.phar')) {
+	$loader = require(__DIR__.'/../website.phar/vendor/autoload.php');
 }
-require('../pxnloader.php');
+if ($loader == null) { echo "Failed to detect autoload.php\n"; exit(1); }
 
-//debug(TRUE);
+// website class
+$website_class = null;
+if (\file_exists(__DIR__.'/.website')) {
+	$website_class = \file_get_contents(__DIR__.'/.website');
+} else
+if (\file_exists(__DIR__.'/../.website')) {
+	$website_class = \file_get_contents(__DIR__.'/../.website');
+}
+if (empty($website_class)) { echo "Failed to detect website class\n"; exit(1); }
+$website_class = \explode("\n", $website_class, 2);
+$website_class = \reset($website_class);
+if (empty($website_class)) { echo "Invalid website class\n"; exit(1); }
+if (!\class_exists($website_class)) { echo "Website class not found\n"; exit(1); }
 
-\pxn\exampleWebsite\exampleWebsite::autoinit();
-*/
+// load website
+$website = new $website_class($loader);
+$website->run();
