5682fc8fca2d81a304969e62296eac1e08b15c9e
lorenzop <lorenzo@poixson.com>
2022-08-12 06:19:43 -0400

    improved handling and parsing of .website file in public/index.php

--- public-index.php
+++ public-index.php
@@ -20,9 +20,13 @@ if (\file_exists(__DIR__.'/../.website')) {
 }
 if (empty($website_class)) { echo "Failed to detect website class\n"; exit(1); }
 $website_class = \explode("\n", $website_class, 2);
-$website_class = \reset($website_class);
+$website_class = \trim(\reset($website_class));
+if (\str_ends_with($website_class, '\\'))
+	$website_class = \mb_substr($website_class, 0, -1);
 if (empty($website_class)) { echo "Invalid website class\n"; exit(1); }
-if (!\class_exists($website_class)) { echo "Website class not found\n"; exit(1); }
+if (!\str_ends_with($website_class, '\\Website'))
+	$website_class .= '\\Website';
+if (!\class_exists($website_class)) { echo "Website class not found: $website_class\n"; exit(1); }
 
 // load website
 $website = new $website_class($loader);
