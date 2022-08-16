a4f8bf929289ca4fcc5785527bbffe791e6cb666
lorenzop <lorenzo@poixson.com>
2022-08-16 17:00:23 -0400

    added new getMarkdownConverter() static function to Wiki class

--- src/pages/Wiki.php
+++ src/pages/Wiki.php
@@ -8,8 +8,36 @@
  */
 namespace pxn\phpPortal\pages;
 
+use \League\CommonMark\MarkdownConverter;
+
 
 abstract class Wiki extends \pxn\phpPortal\Page {
+
+
+
+	public static function getMarkdownConverter(): MarkdownConverter {
+		$cfg = [
+			'external_link' => [
+				'internal_hosts' => 'offgrid.local',
+				'open_in_new_window' => true,
+				'html_class' => 'external-link',
+				'nofollow'   => '',
+				'noopener'   => '',
+				'noreferrer' => '',
+			]
+		];
+		$env = new \League\CommonMark\Environment\Environment($cfg);
+		$env->addExtension( new \League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension()       );
+		$env->addExtension( new \League\CommonMark\Extension\Attributes\AttributesExtension()           );
+		$env->addExtension( new \League\CommonMark\Extension\DescriptionList\DescriptionListExtension() );
+		$env->addExtension( new \League\CommonMark\Extension\ExternalLink\ExternalLinkExtension()       );
+		$env->addExtension( new \League\CommonMark\Extension\Autolink\AutolinkExtension()               );
+		$env->addExtension( new \League\CommonMark\Extension\Strikethrough\StrikethroughExtension()     );
+		$env->addExtension( new \League\CommonMark\Extension\Table\TableExtension()                     );
+		$env->addExtension( new \League\CommonMark\Extension\TaskList\TaskListExtension()               );
+		$converter = new MarkdownConverter($env);
+		return $converter;
+	}
 
 
 
