aa335961e8459dff6ccaeb1f1fba53d71b732850
lorenzop <lorenzo@poixson.com>
2022-08-25 16:25:10 -0400

    cleaned up and finished Wiki page class

--- src/pages/Wiki.php
+++ src/pages/Wiki.php
@@ -11,13 +11,21 @@ namespace pxn\phpPortal\pages;
 use \League\CommonMark\MarkdownConverter;
 use \League\CommonMark\Extension as ComMarkExt;
 
+use \pxn\phpUtils\utils\PathUtils;
+
 
 abstract class Wiki extends \pxn\phpPortal\Page {
 
+	protected ?string $wiki_name = null;
+	protected ?string $wiki_file = null;
 
 
-	public static function getMarkdownConverter(): MarkdownConverter {
+
+	public static function getMarkdownConverter(string $wiki_name): MarkdownConverter {
 		$cfg = [
+			'internal_link' => [
+				'base_uri' => "/$wiki_name",
+			],
 			'external_link' => [
 				'internal_hosts' => 'offgrid.local',
 				'open_in_new_window' => true,
@@ -36,32 +44,67 @@ abstract class Wiki extends \pxn\phpPortal\Page {
 		$env->addExtension( new ComMarkExt\Strikethrough\StrikethroughExtension()     );
 		$env->addExtension( new ComMarkExt\Table\TableExtension()                     );
 		$env->addExtension( new ComMarkExt\TaskList\TaskListExtension()               );
+//TODO
+//		$env->addExtension( new \pxn\phpPortal\pages\wiki\InternalLinkExtension()     );
+//		$env->addExtension( new \pxn\phpPortal\pages\wiki\BracketLinkExtension()      );
 		$converter = new MarkdownConverter($env);
 		return $converter;
 	}
 
 
 
-	public function __construct(\pxn\phpPortal\WebApp $app) {
+	public function __construct(\pxn\phpPortal\WebApp $app, string $wiki_name=null) {
 		parent::__construct($app);
+		$this->wiki_name = $wiki_name;
+	}
+
+
+
+	public function getDefaultPage(): string {
+		return 'home';
+	}
+
+	public function getFile(): string {
+		if (empty($this->wiki_file)) {
+			$file = \implode('/', $this->app->args);
+			// default
+			if (empty($file))
+				$file = $this->getDefaultPage();
+			$file = \implode('/', [
+				$this->app->paths['data'],
+				$this->wiki_name,
+				$file,
+			]);
+			if (!\str_ends_with($file, '.txt'))
+				$file .= '.txt';
+			// check safe path
+			$file = PathUtils::NormPath($file);
+			if (!\str_starts_with($file, $this->app->paths['data']))
+				throw new \RuntimeException("Invalid wiki path: $file");
+			$this->wiki_file = $file;
+		}
+		return $this->wiki_file;
+	}
+
+	public function isValidPage(): bool {
+		return \is_file( $this->getFile() );
 	}
 
 
 
 	public function render(): void {
 		$converter = self::getMarkdownConverter();
-//TODO:
-		$file = $this->app->paths['data'].'/wiki/home.txt';
+		$file = $this->getFile();
 		$content = \file_get_contents($file);
 		if ($content === false)
-			throw new \RuntimeException('Failed to load wiki file: '.$file);
+			throw new \RuntimeException("Failed to load wiki file: $file");
 		$rendered_content = $converter->convertToHtml($content);
 		$twig = $this->getTwig();
 		$tags = [
-			'body' => 'wiki.twig',
-			'content' => $rendered_content,
+			'content' => $rendered_content->getContent(),
 		];
-		echo $twig->render('main.twig', $tags);
+		$page_content = $twig->render('wiki.twig', $tags);
+		echo $this->render_main($page_content);
 	}
 
 
