9956630fbef094411ee12a9920088f766a268787
lorenzop <lorenzo@poixson.com>
2022-08-25 16:25:42 -0400

    cleaned and finished StaticTwig page class

--- src/pages/StaticTwig.php
+++ src/pages/StaticTwig.php
@@ -8,26 +8,55 @@
  */
 namespace pxn\phpPortal\pages;
 
+use \pxn\phpUtils\utils\PathUtils;
+
 
 abstract class StaticTwig extends \pxn\phpPortal\Page {
 
-	protected string $tplFile;
+	protected string $page_file;
+	protected ?string $tpl_file = null;
 
 
 
-	public function __construct(\pxn\phpPortal\WebApp $app, string $tplFile) {
+	public function __construct(\pxn\phpPortal\WebApp $app, string $page_file) {
 		parent::__construct($app);
-		$this->tplFile = $tplFile;
+		$this->page_file = $page_file;
+	}
+
+
+
+	public function getFile(): string {
+		if (empty($this->tpl_file)) {
+			$file = \implode('/', [
+				$this->app->paths['html'],
+				$this->page_file,
+			]);
+			if (!\str_ends_with($file, '.twig'))
+				$file .= '.twig';
+			// check safe path
+			$file = PathUtils::NormPath($file);
+			if (!\str_starts_with($file, $this->app->paths['html']))
+				throw new \RuntimeException("Invalid page path: $file");
+			$this->tpl_file = $file;
+		}
+		return $this->tpl_file;
+	}
+
+	public function isValidPage(): bool {
+		return \is_file( $this->getFile() );
 	}
 
 
 
 	public function render(): void {
+		$file = $this->getFile();
+		$f = PathUtils::TrimPath($file, $this->app->paths['html']);
+		if ($f === false)
+			throw new \RuntimeException("Invalid page path: $file");
 		$twig = $this->getTwig();
-		$tags = [
-			'body' => 'pages/home.twig',
-		];
-		echo $twig->render('main.twig', $tags);
+		$tags = [];
+		$content = $twig->render($f, $tags);
+		echo $this->render_main($content);
 	}
 
 
