177ccec5e883fad1a134c8070cfcfdb410135e51
lorenzop <lorenzo@poixson.com>
2022-08-31 05:26:55 -0400

    cleaned up Blog.php page class

--- src/pages/Blog.php
+++ src/pages/Blog.php
@@ -11,20 +11,24 @@ namespace pxn\phpPortal\pages;
 
 abstract class Blog extends \pxn\phpPortal\Page {
 
+	protected string $blog_name = '';
 
 
-	public function __construct(\pxn\phpPortal\WebApp $app) {
+
+	public function __construct(\pxn\phpPortal\WebApp $app, string $blog_name='') {
 		parent::__construct($app);
+		$this->blog_name = $blog_name;
 	}
 
 
 
 	public function render(): void {
+		$converter = Wiki::getMarkdownConverter();
 		$twig = $this->getTwig();
 		$tags = [
-			'body' => 'blog.twig',
+			'debug' => Debug::debug(),
 		];
-		echo $twig->render('main.twig', $tags);
+		echo $twig->render('blog.twig', $tags);
 	}
 
 
