66b68fd8959ce6c7ab4c6b0c7bf494506ffe2f8f
lorenzop <lorenzo@poixson.com>
2023-02-05 14:20:35 -0500

    added PageAPI abstract class

---
+++ src/PageAPI.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+/*
+ * PoiXson phpPortal - Website Utilities Library
+ * @copyright 2004-2022
+ * @license AGPL-3
+ * @author lorenzo at poixson.com
+ * @link https://poixson.com/
+ */
+namespace pxn\phpPortal;
+
+
+abstract class PageAPI extends Page {
+
+
+
+	public function getName(): string {
+		return 'api';
+	}
+
+
+
+	public function isDefaultPage(): bool {
+		return $this->app->is_api;
+	}
+
+
+
+	public function getActiveWeight(): int {
+		if ($this->app->is_api)
+			return 99;
+		return parent::getActiveWeight();
+	}
+
+
+
+}
